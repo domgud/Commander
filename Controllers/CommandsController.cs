892b6dcd79ab9f626c492ce5d64a9c0ac219468a
domgud <domantas.gudonis@ktu.edu>
2021-02-20 22:31:20 +0200

    Initial commit

---
+++ Controllers/CommandsController.cs
@@ -0,0 +1,37 @@
+﻿using Commander.Data;
+using Commander.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Controllers
+{
+    [Route("api/commands")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+        
+    {
+        private readonly ICommanderRepo _repository;
+
+        public CommandsController(ICommanderRepo repository)
+        {
+            _repository = repository;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GetAllComands();
+            return Ok(commandItems);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandbyId(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+            return Ok(commandItem);
+        }
+    }
+}
---
+++ Data/MockCommanderRepo.cs
@@ -0,0 +1,27 @@
+﻿using Commander.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAllComands()
+        {
+            var commands = new List<Command>
+            {
+                new Command(0, "Boil an egg", "Boil water", "Pan"),
+                new Command(1, "CutBread", "Get knife", "Knife"),
+                new Command(2, "Make cup of tea", "Place teabag", "Cup")
+        };
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command(0, "Boil an egg", "Boil water", "Pan");
+        }
+    }
+}
